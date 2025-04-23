35fda88359244537ad8555abdc4e99ee96ad5065
Thành Vũ <tawoz131105@gmail.com>
2025-04-23 10:38:30 +0700

    vdmt-update

---
+++ VdmtLesson1Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VdmtLesson1Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
